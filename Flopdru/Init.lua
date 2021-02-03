522b6469cb48940c6ff484740d31b8c23ed54791
Sylvain Martens <sylvainmartens@gmail.com>
2021-02-03 08:11:39 -0500

    Proper nil checks.

--- Flopdru/Init.lua
+++ Flopdru/Init.lua
@@ -4,19 +4,23 @@ local Flop = select(2, ...) ---@type ns @The addon namespace.
 function Flop.OnEvent(self, event, ...)
     if event == "ADDON_LOADED" and select(1, ...) == FlopAddonName then
         --Hide Macros Binding
-        for i = 1, 12 do _G["MultiBarLeftButton"..i.."HotKey"]:SetAlpha(0) end
-        for i = 1, 12 do _G["MultiBarRightButton"..i.."HotKey"]:SetAlpha(0) end
-        for i = 1, 12 do _G["ActionButton"..i.."HotKey"]:SetAlpha(0) end
-        for i = 1, 12 do _G["MultiBarBottomRightButton"..i.."HotKey"]:SetAlpha(0) end
-        for i = 1, 12 do _G["BonusActionButton"..i.."HotKey"]:SetAlpha(0) end
-        for i = 1, 12 do _G["MultiBarBottomLeftButton"..i.."HotKey"]:SetAlpha(0) end
+        for i = 1, 12 do if _G["MultiBarLeftButton"..i.."HotKey"] ~= nil then _G["MultiBarLeftButton"..i.."HotKey"]:SetAlpha(0) end end
+        for i = 1, 12 do if _G["MultiBarRightButton"..i.."HotKey"] ~= nil then _G["MultiBarRightButton"..i.."HotKey"]:SetAlpha(0) end end
+        for i = 1, 12 do if _G["ActionButton"..i.."HotKey"] ~= nil then _G["ActionButton"..i.."HotKey"]:SetAlpha(0) end end
+        for i = 1, 12 do if _G["MultiBarBottomRightButton"..i.."HotKey"] ~= nil then _G["MultiBarBottomRightButton"..i.."HotKey"]:SetAlpha(0) end end
+        for i = 1, 12 do if _G["BonusActionButton"..i.."HotKey"] ~= nil then _G["BonusActionButton"..i.."HotKey"]:SetAlpha(0) end end
+        for i = 1, 12 do if _G["MultiBarBottomLeftButton"..i.."HotKey"] ~= nil then _G["MultiBarBottomLeftButton"..i.."HotKey"]:SetAlpha(0) end end
         --Enlarge Buff/Debuff,
-        BuffFrame:ClearAllPoints()
-        BuffFrame:SetScale(1.2)
-        BuffFrame:SetPoint("BOTTOMLEFT",PlayerFrame,"BOTTOMLEFT",800,175)
-        BuffFrame.SetPoint = function() end
+        if BuffFrame ~= nil then
+            BuffFrame:ClearAllPoints()
+            BuffFrame:SetScale(1.2)
+            BuffFrame:SetPoint("BOTTOMLEFT",PlayerFrame,"BOTTOMLEFT",800,175)
+            BuffFrame.SetPoint = function() end
+        end
         --Smaller friendly nameplates
-        C_NamePlate.SetNamePlateFriendlySize(60, 30)
+        if C_NamePlate ~= nil then
+            C_NamePlate.SetNamePlateFriendlySize(60, 30)
+        end
     end
 end
 
